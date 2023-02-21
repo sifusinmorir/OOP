822bb38bb9ea3cf347cd92157ecf8cddc6b548a9
Sinue <117844774+sifusinmorir@users.noreply.github.com>
2023-02-21 12:56:50 -0600

    Add files via upload

---
+++ EVA1_9_MENSAJE/src/eva1_9_mensage/EVA1_9_MENSAGE.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_9_mensage;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_9_MENSAGE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        nPrintln("hola",5);
+    }
+    public static void nPrintln(String mensage, int num){
+        for(int i = 0; 1 < num; i++){
+            System.out.println(mensage);
+    }
+    }
+}
